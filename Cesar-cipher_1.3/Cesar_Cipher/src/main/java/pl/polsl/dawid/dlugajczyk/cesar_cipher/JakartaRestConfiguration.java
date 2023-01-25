0edea10e2c62b2259a270bc9a4aede13f69b8a09
Dawid-Dlugajczyk <72344087+Dawid-Dlugajczyk@users.noreply.github.com>
2023-01-25 09:23:00 +0100

    Cesar cipher as web application
    Web application on Payara server

---
+++ Cesar-cipher_1.3/Cesar_Cipher/src/main/java/pl/polsl/dawid/dlugajczyk/cesar_cipher/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package pl.polsl.dawid.dlugajczyk.cesar_cipher;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
